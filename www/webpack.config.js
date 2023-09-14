f8b7528396fae0c0d958511a715993074c44463b
gfelber <@users.noreply.github.com>
2023-09-14 12:21:01 +0200

    added font file

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -14,7 +14,10 @@ module.exports = {
       patterns: [
         { from: "main.css", to: "main.css" },
         { from: "node_modules/xterm/css/xterm.css", to: "xterm.css" },
+        { from: "node_modules/@fontsource-variable/source-code-pro/index.css", to: "font.css" },
         { from: "index.html", to: "index.html" },
+        { from: "index.html", to: "404.html" },
+        { from: "img", to: "img" },
       ],
     }),
   ],
